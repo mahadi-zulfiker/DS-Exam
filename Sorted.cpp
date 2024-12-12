697f98ae23a083438659460bf5d339686587b91a
mahade <mahade.adib45@gmail.com>
2024-12-12 22:07:04 +0600

    sorted it problem solved

--- Sorted.cpp
+++ Sorted.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main(){
-    
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N;
+        cin >> N;
+
+        vector<int> A(N);
+        for(int i = 0; i < N; i++){
+            cin >> A[i];
+        }
+
+        bool isSorted = true;
+        for(int i = 1; i < N; i++){
+            if(A[i] < A[i - 1]){
+                isSorted = false;
+                break;
+            }
+        }
+
+        if(isSorted){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
