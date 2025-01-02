92e33bc1e27acb1e3e7a0fd44c5322543aff6c6a
mahade <mahade.adib45@gmail.com>
2025-01-02 22:20:34 +0600

    palindrome problem solved

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main() {
+    list<int> doublyLinkedList;
+    int value;
+
+    while (cin >> value && value != -1) {
+        doublyLinkedList.push_back(value);
+    }
+
+    auto front = doublyLinkedList.begin();
+    auto back = doublyLinkedList.rbegin();
+    bool isPalindrome = true;
+
+    while (front != doublyLinkedList.end() && back != doublyLinkedList.rend()) {
+        if (*front != *back) {
+            isPalindrome = false;
+            break;
+        }
+        ++front;
+        ++back;
+    }
+
+    if (isPalindrome) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
