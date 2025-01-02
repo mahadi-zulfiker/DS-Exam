9ff6f1efeab90093ecddee4b813867958b1b48d4
mahade <mahade.adib45@gmail.com>
2025-01-02 23:37:03 +0600

    remeber previous queries

--- Remember_Previous_Queries.cpp
+++ Remember_Previous_Queries.cpp
@@ -1,7 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int Q;
+    cin >> Q;
+
+    list<int> dll;
+
+    while (Q--) {
+        int X, V;
+        cin >> X >> V;
+
+        if (X == 0) {
+            dll.push_front(V);
+        } else if (X == 1) {
+            dll.push_back(V);
+        } else if (X == 2) {
+            if (V >= 0 && V < dll.size()) {
+                auto it = dll.begin();
+                advance(it, V);
+                dll.erase(it);
+            }
+        }
+
+        cout << "L -> ";
+        for (int val : dll) {
+            cout << val << " ";
+        }
+        cout << endl;
+
+        cout << "R -> ";
+        for (auto it = dll.rbegin(); it != dll.rend(); ++it) {
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
