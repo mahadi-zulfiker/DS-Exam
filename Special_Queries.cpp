32e58c583f49086d49bb3662b19283e11420a7d0
mahade <mahade.adib45@gmail.com>
2025-01-10 00:02:14 +0600

    special_queries problem sollved

--- Special_Queries.cpp
+++ Special_Queries.cpp
@@ -1,7 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int q;
+    cin >> q;
+
+    queue<string> ticketLine;
+
+    while (q--) {
+        int command;
+        cin >> command;
+
+        if (command == 0) {
+            string name;
+            cin >> name;
+            ticketLine.push(name);
+        } else if (command == 1) {
+            if (!ticketLine.empty()) {
+                cout << ticketLine.front() << endl;
+                ticketLine.pop();
+            } else {
+                cout << "Invalid" << endl;
+            }
+        }
+    }
+
     return 0;
 }
