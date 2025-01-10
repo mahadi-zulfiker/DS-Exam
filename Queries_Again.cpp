461136482a49cc720866af35ee2557057e12b657
mahade <mahade.adib45@gmail.com>
2025-01-10 11:36:12 +0600

    queries again problem solved with raw doubly linked list

--- Queries_Again.cpp
+++ Queries_Again.cpp
@@ -1,8 +1,82 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void printLeftToRight(Node* head) {
+    cout << "L -> ";
+    Node* temp = head;
+    while (temp) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void printRightToLeft(Node* tail) {
+    cout << "R -> ";
+    Node* temp = tail;
+    while (temp) {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+bool insertAtIndex(Node*& head, Node*& tail, int index, int value) {
+    Node* newNode = new Node(value);
+
+    if (index == 0) {
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        return true;
+    }
+
+    Node* temp = head;
+    int currentIndex = 0;
+
+    while (temp && currentIndex < index - 1) {
+        temp = temp->next;
+        currentIndex++;
+    }
+
+    if (!temp || (currentIndex < index - 1)) {
+        delete newNode;
+        return false;
+    }
+
+    if (!temp->next) {
+        temp->next = newNode;
+        newNode->prev = temp;
+        tail = newNode;
+    } else {
+        newNode->next = temp->next;
+        temp->next->prev = newNode;
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    return true;
+}
 
 int main() {
-    list<int> dl;
+    Node* head = NULL;
+    Node* tail = NULL;
+
     int Q;
     cin >> Q;
 
@@ -10,43 +84,18 @@ int main() {
         int X, V;
         cin >> X >> V;
 
-        if (X < 0 || X > dl.size()) {
+        if (insertAtIndex(head, tail, X, V)) {
+            printLeftToRight(head);
+            printRightToLeft(tail);
+        } else {
             cout << "Invalid" << endl;
-            continue;
         }
+    }
 
-        int count = 0;
-        bool inserted = false;
-        list<int> tmpL;
-
-        for (int val : dl) {
-            if (count == X) {
-                tmpL.push_back(V);
-                inserted = true;
-            }
-            tmpL.push_back(val);
-            count++;
-        }
-
-        if (!inserted && X == dl.size()) {
-            tmpL.push_back(V);
-        }
-
-        dl = tmpL;
-
-        cout << "L -> ";
-        for (int val : dl) {
-            cout << val << " ";
-        }
-        cout << endl;
-
-        cout << "R -> ";
-        list<int> reverse_dl(dl);
-        reverse(reverse_dl.begin(), reverse_dl.end());
-        for (int val : reverse_dl) {
-            cout << val << " ";
-        }
-        cout << endl;
+    while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
     }
 
     return 0;
